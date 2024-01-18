92a1dd476129e4844c113f7e1cf038c69daeeb7a
TeeJude <teejayjude@gmail.com>
2024-01-18 20:23:15 +0000

    0-minoperations.py ls

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,8 +7,8 @@ needed to result in exactly n H characters in the file.
 
 def minOperations(n):
     """
-    Returns the fewest number of operations needed to result in exactly
-    n H characters in the file.
+    Returns an integer
+    or 0 if n is impossible to achieve.
     """
     operations = 0
     min_operations = 2
