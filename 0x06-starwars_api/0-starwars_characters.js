3cb14639a7a10fe93c7971885877809462cde183
TeeJude <teejayjude@gmail.com>
2024-02-15 21:45:54 +0000

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require('request');
 const movieId = process.argv[2];
+const request = require('request');
 const options = {
   url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
   method: 'GET'
